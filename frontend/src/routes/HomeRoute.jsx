2498749e224abd9b2b73e1b9dfd571bc7d9a59e8
Tandra Malm <tandra_malm@hotmail.com>
2024-01-12 21:27:51 -0700

    added mocks

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
+import photos from '../mocks/photos';
+import topics from '../mocks/topics';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics}/>
+      <PhotoList photos={photos}/>
     </div>
   );
 };
