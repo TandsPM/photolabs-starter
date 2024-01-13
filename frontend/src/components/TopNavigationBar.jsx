7c410d3a3de7066001b872e75a31b51408fa99ef
Tandra Malm <tandra_malm@hotmail.com>
2024-01-13 15:22:18 -0700

    simplified TNB function

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,21 +4,12 @@ import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = ({ topics, favoriteCount }) => {
-  const [favorited, setFavorited] = useState(false);
-
-  useEffect(() => {
-    if (favoriteCount > 0) {
-      setFavorited(true);
-    } else {
-      setFavorited(false);
-    }
-  }, [favoriteCount]);
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavBadge showBadge={favorited} favoriteCount={favoriteCount}/>
+      <FavBadge showBadge={favoriteCount > 0} favoriteCount={favoriteCount}/>
     </div>
   )
 }
