454396f6ecc27fae0b3c1bc88874d7d5740e1f31
Tandra Malm <tandra_malm@hotmail.com>
2024-01-13 16:19:23 -0700

    added blank modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 import photos from './mocks/photos';
 import topics from './mocks/topics';
 import './App.scss';
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics} />
+      <PhotoDetailsModal />
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal)
+  };
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button onClick={toggleModal} className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
