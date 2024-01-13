9b726080c2c667bbe5603285c1cd90dcd42206af
Tandra Malm <tandra_malm@hotmail.com>
2024-01-12 21:18:04 -0700

    cleaned up app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,12 @@
 import React, { useState } from 'react';
-import PhotoFavButton from'./components/PhotoFavButton';
-import PhotoList from './components/PhotoList';
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 
 const App = () => {
-  const [selected, setSelected] = useState(false);
-
-  const switchFav = () => {
-    setSelected((prevSelected) => !prevSelected);
-  };
 
   return (
     <div className="App">
       <HomeRoute />
-      {/* {[1, 2, 3].map((id) => (
-        <div key={id} className="photo-container">
-          <PhotoFavButton selected={selected} switchFav={switchFav} />
-          <PhotoList />
-        </div>
-      ))} */}
     </div>
   );
 };
