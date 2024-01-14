a29303d3dcdd52a0c754a627c89b15e4f95dde91
Tandra Malm <tandra_malm@hotmail.com>
2024-01-13 17:49:44 -0700

    fixed how profile, user, and location showup on the picture block

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -14,15 +14,23 @@ const PhotoListItem = ({ photo, switchFav, setDisplayModal }) => {
   return (
     <div className="photo-list__item" onClick={handleClick}>
       <PhotoFavButton switchFav={switchFav} isFavorite={photo.isFavorite} photoId={photo.id} />
+
+      <div className="photo-list__content">
       {urls && <img src={urls.regular} alt={`Photo by ${user.username}`} className="photo-list__image"/>}
-      {user && <img src={user.profile} alt={`${user.username}'s profile`} className="photo-list__user-profile"/>}
-      <div className="photo-list__user-details">
-        <div className="photo-list__user-info">
-        <p>{user.username}</p>
-        {location && (
-        <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
+
+      {user && (
+          <div className="photo-list__user-container">
+            <div className="photo-list__user-details">
+              <img src={user.profile} alt={`${user.username}'s profile`} className="photo-list__user-profile" />
+              <div>
+                <p className="photo-list__user-username">{user.username}</p>
+                {location && (
+                  <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
+                )}
+              </div>
+            </div>
+          </div>
         )}
-        </div>
       </div>
     </div>
   )
